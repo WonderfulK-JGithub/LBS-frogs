5da0839a7b342fb395eff92c987b02977de07231
WonderfulK-JGithub <70464858+WonderfulK-JGithub@users.noreply.github.com>
2021-03-02 16:15:41 +0100

    Skapat Enemy skript, inte klar

---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    //En enumerator som är till för att veta vilken slags fiende det är
+    public enum EnemyType
+    {
+        pig,
+        chicken,
+        farmer
+    }
+
+    [Header("Type of enemy")]
+    //variabel för vilken typ av fiende det är
+    public EnemyType type;
+
+    [Header("Enemy stats")]
+
+    public int healthPoints;
+    [SerializeField] private int damage;
+
+    [Header("Movement")]
+    [SerializeField] private float spd;
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
