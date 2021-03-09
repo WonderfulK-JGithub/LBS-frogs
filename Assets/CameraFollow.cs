1ca80b793fc2d38cd916eb834aca2ec23c2faac0
Strutsiboi <melker.johansson@elev.ga.lbs.se>
2021-03-09 11:09:18 +0100

    Kamera följer spelare och Fiender pattrulerar
    Kameran följer spelaren och Fiender kan pattrulera. OBS: Fiender gör det bäst om de står på en platform. Om de står på en vanlig mark kommer den att gå hela vägen fram och tillbaka.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    //Den här offset är viktig för att spelarens z-axel är på 0, medans cameran är på -10
+    public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow ()
+    {
+        //Vi tar positionen av spelaren + offset.
+        Vector3 targetPosition = player.position + offset;
+        //Den här gör att kameran väntar en liten stund innan den flyttar sig till spelaren. 
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = targetPosition;
+    }
+}
---
+++ Assets/Patrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    //Håller kåll så att fienden inte ramlar ner
+    public float distance;
+    
+    private bool movingRight = true;
+    //Håller kåll på om det finns mark framför sig.
+    public Transform groundDetection;
+
+    void Update()
+    {
+        //Hur fungerar detta makalösa script? Tänk dig att det finns en onsynlig pinne framför spelaren några millimeter bort. 
+        //Denna pinne håller kåll på om det finns mark som fienden kan gå på. Om det finns, så ska fienden fortsätta gå. Men om det inte finns någon mark, så ska fienden vrida sig 180 grader, alltså gå åt vänster.
+        //Samma sak gäller när den går åt vänster. När det inte finns någon mark kvar så ska fienden vända och gå till höger.
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+
+}
