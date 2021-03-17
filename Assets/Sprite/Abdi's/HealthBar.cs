d1af46ee40bb4184e48c2537fefbe18e425932b2
abdilatif1455 <79506212+abdilatif1455@users.noreply.github.com>
2021-03-17 14:39:30 +0100

    health Bar

---
+++ Assets/Sprite/Abdi's/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image Fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        Fill.color = gradient.Evaluate(1f);// vi kan få en färg från vår gradient vid en specifik punkt
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        Fill.color = gradient.Evaluate(slider.normalizedValue);// När vi tar damage och vill ställa vår health till nytt
+    }
+
+
+}
---
+++ Assets/Sprite/Abdi's/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;// När vi startar spelet kommer vår health var Max
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);// varje gång vi klicker på space tar vi damage 20 
+        }
+
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;// subtrahera skador från vår nuvarande current health
+
+        healthBar.SetHealth(currentHealth);// uppdateringar varje gång vi tar skada
+    }
+}
