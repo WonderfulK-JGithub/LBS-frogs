0f4e7bde51445a9717e7e7dc76464883dc47a6c1
Max <79506271+max-aug@users.noreply.github.com>
2021-03-30 10:17:54 +0200

    man kan ändra volym

---
+++ Assets/MenuManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManagerScript : MonoBehaviour
+{
+    public GameObject settingsObj;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        if (!FindObjectOfType<SettingsDataScript>())
+        {
+            Instantiate(settingsObj, Vector2.zero, Quaternion.identity);
+        }
+
+        SettingsDataScript settingsDataObj = FindObjectOfType<SettingsDataScript>();
+        volumeSlider.value = settingsDataObj.volume;
+        volumeSlider.onValueChanged.AddListener(delegate { settingsDataObj.ChangeVolume(volumeSlider.value); });
+    }
+
+}
